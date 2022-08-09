c19c5585c4e69d6b0f0b42acf9d06fb35028b405
burhan yiğiter <burhan.yigiter@gmail.com>
2022-08-09 22:08:48 +0300

    Data_types

---
+++ Front_End/javascript/s02/app.js
@@ -0,0 +1,93 @@
+console.log("App js running");
+
+// var a = 5;
+// console.log(a);
+
+// {
+//     var a =3;
+//     console.log(a);
+// }
+
+// console.log(a);
+
+// let a = 5;
+// console.log(a);
+
+// { // başına let a = 3; dersek aşağısı etkilenmez. sadece burası etkilenir.
+    // a = 3;  // atama yapdığımız için burası globali de etkiliyor.
+    // aşağısı da buradan ekleniyor.
+//     console.log(a);
+// }
+
+// console.log(a);
+
+/* console.log(typeof 0);2
+console.log(typeof 3.14);
+console.log(typeof 'Hello');
+console.log(typeof 8 > 10);
+console.log(8 > 10);12
+console.log(typeof ''); */
+
+// let b = +prompt('Bir şey giriniz');  // bu artı işreti girilen değer string alıyor ama bir rakamsa bunu o artı işareti rakama  dönüştürüyor.
+
+// console.log(b, typeof b);123
+
+
+// console.log(typeof (typeof 42));
+
+/* 
+string olan sayı ifadesini
+başına + ekleyip number tipine
+parseInt(stringVar) ile integer number a
+parseFloat(stringVar) ile kesirli sayı
+elde ederiz  
+*/
+
+// let age;
+// console.log(age);  // undefined
+
+// console.log(myAge); // is not defined
+
+// let age;
+// console.log(age);  // undefined
+
+// console.log(myAge); // is not defined
+
+// var myAge = 40;
+// var myAge1 = 40;
+// var myAge2= 40;
+// var myAge3= 40;  // atanmış olan bunlar kullanlmıyor ama cihazıımızda bir yer kaplıyor. oyüzden genel de let kullanmalıyız.
+// var myAge4= 40;
+// console.log('Hello');
+
+// NULL (boşluk veya yokluk)
+
+// let x ;
+// do {5
+//     x = prompt('enter a number : ');
+// }while (isNaN(x));
+
+// console.log(x, x*x);
+
+
+
+// console.log('12' * '12')
+
+
+// let x = 123456789001234
+// let y = 123456789001234123
+
+// let z = 0.12345678901234567
+// let zz = 0.123456789012345675678
+
+// console.log('12' === 12)
+
+// console.log({x})
+// console.log({y},typeof y)
+
+// console.log(z)
+// console.log(zz)
+
+// let g = (0.1 + 0.2);
+// console.log(g)
+// console.log(+g.toFixed(2));
