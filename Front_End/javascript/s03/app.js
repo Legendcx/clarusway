28e77810456aabfb9dec44572684aa3dd83b2a4e
burhan yiğiter <burhan.yigiter@gmail.com>
2022-08-11 20:50:48 +0300

    yap

---
+++ Front_End/javascript/s03/app.js
@@ -0,0 +1,16 @@
+"use scrict"
+
+// OPERATORS
+// userName = "Burhan Ali"
+// let selamlama = `Hoşgeldiniz, ${username}, xyz bankası iyi günler diler`;
+// console.log(selamlama)
+
+
+// comperation operators
+
+console.log(3 == "3");  // true
+console.log(3 != "3");  // false
+console.log(3 === "3");  false
+console.log(3 !== "3");  // true
+console.log(3 <= "2");  // false
+console.log(3 >= "2");  // true
