bc4801ba23a44e682caacc6d2086c4cb663c0857
burhan yiğiter <burhan.yigiter@gmail.com>
2022-08-16 10:26:53 +0300

    değişti

--- Front_End/javascript/s04/conditions.js
+++ Front_End/javascript/s04/conditions.js
@@ -114,9 +114,69 @@ console.log("****** CONDITIONS *******");
 
 //  console.log(notum >= 50 ? "Geçti : "Kaldı");
 
-//? ÖDEV-3 :  Maaşı asgariücrreten düşükse %50 zam alsın, büyükse %10 zam yapmak istiyoruz. maaşı alacağız ve zam yaptıracağız ternary ile 
-//! let maas = 5000;
-//! maas = maaş * 1.5;
+//? ODEV1: Dort Islem Hesap Makinasi
+
+//todo CEVAP-1:
+
+// let num1 = +prompt("Enter first number");
+// let num2 = +prompt("Enter second number");
+// let oparations = prompt("Choose one of the math operations(+,-,*,/)");
+
+// if (oparations === "+") {
+//     console.log(`${num1} + ${num2} = ${num1 + num2}`);
+// } else if (oparations === "-") {
+//     console.log(`${num1} - ${num2} = ${num1 - num2}`);
+// }else if (oparations === "*") {
+//     console.log(`${num1} * ${num2} = ${num1 * num2}`);
+// }else {
+//     console.log(`${num1} / ${num2} = ${(num1 / num2).toFixed(2)}`);
+// }
+
+// let num1 = +prompt("Enter first number");
+// let num2 = +prompt("Enter second number");
+// let oparations = prompt("Choose one of the math operations(+,-,*,/)");
+
+//! CEVAP-1 : SWITCH-CASE SOLUTİON
+// switch (oparations){
+//     case "+" : console.log(`${num1} + ${num2} = ${num1 + num2}`); break;
+//     case "-" : console.log(`${num1} - ${num2} = ${num1 - num2}`); break;
+//     case "*" : console.log(`${num1} * ${num2} = ${num1 * num2}`); break;
+//     default: console.log(`${num1} / ${num2} = ${(num1 / num2).toFixed(2)}`); break;
+// }
+
+
+
+//? ÖDEV-2:
+//? Clarusway’deki haftalık ders ve etkinlik programınızı, console’dan girilen gün değerine göre çıktı veren kodu switch-case yapısı ile yazınız.
+
+//? Program
+//? Pazartesi, Salı ,Çarşamba, Perşembe -> InClass
+//? Cuma -> Teamwork
+//? Cumartesi ->  InClass + Workshop
+//!? Pazar -> Self-Study
+//? Aksi takdirde -> Yanlis gun girildi.
+
+//todo CEVAP-2:
+
+// let days = prompt("Enter a day of week").toLowerCase();
+
+// switch (days) {
+//     case "pazartesi": console.log("InClass"); break;
+//     case "salı": console.log("InClass"); break;
+//     case "çarşamba": console.log("InClass"); break;
+//     case "perşembe": console.log("InClass"); break;
+//     case "cuma": console.log("Teamwork"); break;
+//     case "cumartesi": console.log("In-class + Workshop"); break;
+//     case "pazar": console.log("Self-Study"); break;
+//     default: console.log("Yanlış gün girildi.");break;
+// }
+
+
+//? ÖDEV-3 :  
+//? Maasi asgari ucretten az olanlara %50 zam,
+//? fazla olanlara ise %10 zam yapmak istiyoruz. Ternary ile yapalım...
+//!? let maas = 5000;
+//? maas = maaş * 1.5;
 
 //todo CEVAP-3:
 
@@ -126,65 +186,68 @@ console.log("****** CONDITIONS *******");
 // console.log(`New salary ${newSalary}₺'dir.`);
 
 
-//? ÖDEV-4 : Gelir - Gider miktarını alacagız asgari ücret kadar ise kredi alabilir. fazla ise kredi alamaz.
+//? ÖDEV-4 : 
+//? Console’dan kişinin gelir ve gider miktarını alan bir kd yazıyoruz.
+//? eğer kişinin geliri giderinden en az asgari ücret kadar fazla ise Kredi Verilebilir. aksi halde kredi alamaz. 
 
 //todo CEVAP-4:
 
-const income = +prompt("Enter your income");
-const expense = +prompt("enter your expense");
+ let income = +prompt("Enter your income");
+ let expense = +prompt("enter your expense");
+ let remaining = income - expense;
+ let minSalary = 5500;
 
-const minSalary = 5500;
+let situation = remaining >= minSalary ? "You can get a loan" : "You can't get a loan";
+console.log(situation);
 
 
-// you can get a loan
 
+//! * ===============================================
+//! *     NOAH HOCA'nın BÖLÜMÜ () KARAR YAPILARI)
+//! * ===============================================
 
-// * ===============================================
-// *     NOAH HOCA'nın BÖLÜMÜ () KARAR YAPILARI)
-// * ===============================================
+//todo **************** IF-ELSE *******************
 
-//**************** IF-ELSE *******************
-
-console.log("****** CONDITIONS *******");
+// console.log("****** CONDITIONS *******");
 
 //? ORNEK1: Console'dan girilen bir sayisinin pozitif,negatif
 //? yada 0 oldugunu tespit ederek yazdiriniz.
 
-const sayi = Number(prompt("Bir sayi giriniz:"));
-if (sayi > 0) {
-  console.log(`${sayi} pozitiftir.`);
-} else if (sayi == 0) {
-  console.log(`${sayi}'a esittir.`);
-} else {
-  console.log(`${sayi} negatiftir.`);
-}
-console.log(sayi, typeof sayi);
+// const sayi = Number(prompt("Bir sayi giriniz:"));
+// if (sayi > 0) {
+//   console.log(`${sayi} pozitiftir.`);
+// } else if (sayi == 0) {
+//   console.log(`${sayi}'a esittir.`);
+// } else {
+//   console.log(`${sayi} negatiftir.`);
+// }
+// console.log(sayi, typeof sayi);
 
 //? ORNEK2: console'dan 3 tamsayi alarak bunlarin en buyugunu
 //? yazdiriniz.
-const n1 = +prompt("Sayi1:");
-const n2 = +prompt("Sayi2:");
-const n3 = +prompt("Sayi3:");
+// const n1 = +prompt("Sayi1:");
+// const n2 = +prompt("Sayi2:");
+// const n3 = +prompt("Sayi3:");
 
 //* 1.Yontem
-if (n1 >= n2 && n1 >= n3) {
-  console.log(`${n1} en buyuk sayidir`);
-} else if (n2 >= n1 && n2 >= n3) {
-  console.log(`${n2} en buyuk sayidir`);
-} else if (n3 >= n1 && n3 >= n2) {
-  console.log(`${n3} en buyuk sayidir`);
-}
+// if (n1 >= n2 && n1 >= n3) {
+//   console.log(`${n1} en buyuk sayidir`);
+// } else if (n2 >= n1 && n2 >= n3) {
+//   console.log(`${n2} en buyuk sayidir`);
+// } else if (n3 >= n1 && n3 >= n2) {
+//   console.log(`${n3} en buyuk sayidir`);
+// }
 
 //* 2.Yontem
-let enBuyuk = n1;
+// let enBuyuk = n1;
 
-if (n2 >= enBuyuk) {
-  enBuyuk = n2;
-}
-if (n3 >= enBuyuk) {
-  enBuyuk = n3;
-}
-console.log(`${enBuyuk} en buyuktur`);
+// if (n2 >= enBuyuk) {
+//   enBuyuk = n2;
+// }
+// if (n3 >= enBuyuk) {
+//   enBuyuk = n3;
+// }
+// console.log(`${enBuyuk} en buyuktur`);
 
 
 
@@ -205,19 +268,19 @@ console.log(`${enBuyuk} en buyuktur`);
 //**************** TERNARY *******************
 
 //? ORNEK5: Kaldi-Gecti
-const not = +prompt("Notunuzu giriniz:");
+// const not = +prompt("Notunuzu giriniz:");
 // let sonuc = not >= 50 ? "Gecti" : "Kaldi";
 // console.log(sonuc);
-console.log(not >= 50 ? "Gecti" : "Kaldi");
+// console.log(not >= 50 ? "Gecti" : "Kaldi");
 
 //? ORNEK6:
-const yas = 17;
-const cinsiyet = "erkek";
-const saglikli = false;
+// const yas = 17;
+// const cinsiyet = "erkek";
+// const saglikli = false;
 
-yas >= 18 && cinsiyet === "erkek" && saglikli
-  ? console.log("askerlik yapmali")
-  : console.log("Askerlik yapmasina gerek yok");
+// yas >= 18 && cinsiyet === "erkek" && saglikli
+//   ? console.log("askerlik yapmali")
+//   : console.log("Askerlik yapmasina gerek yok");
 
 //! ODEV3:Maasi asgari ucretten az olanlara %50 zam,
 //! fazla olanlara ise %10 zam yapmak istiyoruz.
@@ -232,4 +295,7 @@ yas >= 18 && cinsiyet === "erkek" && saglikli
 
 
 
-
+// const gelir = +prompt('Aylık Gelirinizi Giriniz');
+// const gider = +prompt('Aylık Giderinizi Giriniz');
+// let aUcret = 6471;
+// console.log((gelir >= gider + aUcret) ? 'Kredi verilebilir' : 'Kredi verilemez')
