18ccd9774c91614739ab4b8e3c58a5279dda9498
burhan yiğiter <burhan.yigiter@gmail.com>
2022-09-24 13:06:13 +0300

    async.bitti

--- Front_End/javascript/s19-Async-Await/async-await/async-await.js
+++ Front_End/javascript/s19-Async-Await/async-await/async-await.js
@@ -20,14 +20,58 @@
 
 
 
+let isError = false;
 
 const getNews = async function (){
     const API_KEY = "f8330b5ab903445588c9318ef341e5f4";
+    const url = "https://newsapi.org/v2/top-headlines?country=tr&apiKey=" + API_KEY;
 
-    const url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=" + API_KEY;
-
+  
+    
+    try {
     const res = await fetch(url);
-    console.log(res);
+    if(!res.ok){
+        isError = true;
+        // throw new Error(`Something went wrong: ${res.status}`);
+
+        //! hatayı fırlatırsak catch te kodu durdurur.
+    }
+    const data = await res.json();
+    console.log(data.articles);
+    renderNews(data.articles);
+//! try and catch kullanmassak orada durur ve hata olarak önümüze gelir.
+    } catch (error) {
+        console.log(error);
+    }    
 };
 
-getNews();
+const renderNews = (news) => {
+    newsList = document.getElementById("news-list");
+    if(isError){
+        newsList.innerHTML += `
+        <h2>News can not be fetched</h2>
+        <img src="./img/404.png" alt="" />`
+    }
+
+    news.forEach((item) => {
+       const {title, description, urlToImage, url} = item;
+       newsList.innerHTML +=`
+       <div class="col-md-6 col-lg-4 col-xl-3">
+       <div class="card">
+  <img src="${urlToImage}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${title}</h5>
+    <p class="card-text">${description}</p>
+    <a href="${url}" target="_blank" class="btn btn-danger">Details</a>
+  </div>
+  </div>
+</div>`
+    })
+    
+}
+
+
+
+window.addEventListener("load", getNews);
+// getNews(); bunun yerine yukarıdaki de yazılabilir.
+
