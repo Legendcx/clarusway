8ea43f7563bd3c47edf6c255bc7d9509939da31a
burhan yiğiter <burhan.yigiter@gmail.com>
2022-10-02 22:33:19 +0300

    g

--- Front_End/javascript/s23_modeles/module2.js
+++ Front_End/javascript/s23_modeles/module2.js
@@ -5,3 +5,5 @@ import {inc} from './mymodule.js';
 console.log('inc(100)', inc(100));
 
 //*Biz hangi modullden import edersek edelim bir tane modül oluyor ve hep o artıp azalacak. hafıza da bir tane modül var.  
+
+console.log(burhan);
