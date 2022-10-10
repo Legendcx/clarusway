334169375c99c06327b035f161af32a70042cf30
burhan yiğiter <burhan.yigiter@gmail.com>
2022-10-10 22:15:54 +0300

    module

---
+++ Front_End/react/class-notes/rc03_module-css/src/components/buton/Buton.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+// bunu yazmaya biliriz.
+
+// import "./Buton.css";
+import ButonStyle from "./buton.module.css";
+
+const Buton = ({ btn }) => {
+  console.log(btn);
+  return (
+    <div className={ButonStyle.title}>
+      <button className={ButonStyle["btn-blue"]}>{btn}</button>
+    </div>
+  );
+};
+
+export default Buton;
