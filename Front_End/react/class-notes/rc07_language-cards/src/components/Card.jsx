61bc049425b5d3a1eb732223995799e62fcdc252
burhan yiğiter <burhan.yigiter@gmail.com>
2022-10-19 00:10:51 +0300

    change

--- Front_End/react/class-notes/rc07_language-cards/src/components/Card.jsx
+++ Front_End/react/class-notes/rc07_language-cards/src/components/Card.jsx
@@ -7,8 +7,8 @@ import {data} from "../helpers/data";
 const Card = () => {
   console.log(data);
   return (
-    <Container className="rounded-3 mt-4 p-4" style={{ background: "#f48b29" }}>
-      <h1 className="text-white my-2">Language</h1>
+    <Container className="rounded-3 mt-4 p-4" style={{ background: "crimson" }}>
+      <h1 className="text-white my-3">LANGUAGE</h1>
       <Row className="g-3 justify-content-center">
        
         {data.map((lang, index)=>{
--- Front_End/react/class-notes/rc07_language-cards/src/components/Language.jsx
+++ Front_End/react/class-notes/rc07_language-cards/src/components/Language.jsx
@@ -3,15 +3,17 @@ import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
 
 const Language = ({name, img, options}) => {
-  const [showImage, setShowImage] = useState(false)
+  const [showImage, setShowImage] = useState(false);
   // console.log(lang);
+  console.log(showImage);
 
-  return <Container style={{background:"peachpuff"}} className="p-4" rounded-2  h-100 lang-card onClick={()=> setShowImage(!showImage)}
-  type="button"
+  return <Container style={{background:"lightyellow"}} className="p-3 rounded-4  h-100 lang-card"
+   onClick={()=> setShowImage(!showImage)}
+  type="button">
 
     {!showImage && (<Container>
     <Image className="" src={img} width="70%"></Image>
-    <h3 className="display-6">{name}</h3>
+    <h3 className="fs-2 fw-bold text-danger mt-2">{name}</h3>
   </Container>)}
 
   {showImage && ( <ol className="h-100 d-flex flex-column justify-content-center">
