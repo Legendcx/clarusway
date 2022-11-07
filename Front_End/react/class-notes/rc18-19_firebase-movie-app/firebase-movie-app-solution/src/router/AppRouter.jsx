866873e194f7c01c486b3b97dc988736683c23e6
burhan yiğiter <burhan.yigiter@gmail.com>
2022-11-07 23:19:39 +0300

    hhh

--- Front_End/react/class-notes/rc18-19_firebase-movie-app/firebase-movie-app-solution/src/router/AppRouter.jsx
+++ Front_End/react/class-notes/rc18-19_firebase-movie-app/firebase-movie-app-solution/src/router/AppRouter.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Main from "../pages/Main";
+import Navbar from "../components/Navbar";
 import Login from "../pages/Login";
-import Register from "../pages/Register";
+import Main from "../pages/Main";
 import MovieDetail from "../pages/MovieDetail";
+import Register from "../pages/Register";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login/>} />
-        <Route path="/register" element={<Register/>}/>
-        <Route path="/details/:id" element={<MovieDetail/>}/>
-
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/details/:id" element={<MovieDetail />} />
       </Routes>
     </BrowserRouter>
   );
