a363c2e2b78c5318424275bfac4f28a78f4dd7b5
burhan yiğiter <burhan.yigiter@gmail.com>
2022-11-10 21:49:44 +0300

    redux

---
+++ Front_End/react/class-notes/rc21_redux-intro/redux-intro-inClass/src/components/counter/Counter.js
@@ -0,0 +1,20 @@
+import "./Counter.css";
+import { useDispatch, useSelector } from "react-redux";
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
+  return (
+    <div className="app">
+      <h2 className="counter-header">Counter With Redux</h2>
+      <h1>counter:0</h1>
+      <div>
+        <button className="counter-button positive" onClick={() => dispatch({type: "INCREMENT"})}>increase</button>
+        <button className="counter-button zero" onClick={() => dispatch({type: "CLEAR"})}>reset</button>
+        <button className="counter-button negative" onClick={() => dispatch({type: "DICREMENT"})}>decrease</button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
