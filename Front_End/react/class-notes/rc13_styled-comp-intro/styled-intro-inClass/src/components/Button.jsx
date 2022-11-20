b3c235b775c5e70b954f7c544da3649d9aa99d2e
burhan yiğiter <burhan.yigiter@gmail.com>
2022-11-20 23:21:16 +0300

    ff

--- Front_End/react/class-notes/rc13_styled-comp-intro/styled-intro-inClass/src/components/Button.jsx
+++ Front_End/react/class-notes/rc13_styled-comp-intro/styled-intro-inClass/src/components/Button.jsx
@@ -9,13 +9,13 @@ background-color: ${({primary}) => (primary ? "purple" : "aqua")};
 color: white;
 cursor: pointer;
 width: 8rem;
-padding: 1rem 2rem;
-border-radius: 0.5rem;
+padding: 1.1rem 2rem;
+border-radius: 0.6rem;
 border-color: purple;
 font-size: 1.2rem;
 font-family: 'Segoe UI', sans-serif;
 &:hover{
-  transform: scale(0.97);
+  transform: scale(0.96);
 }
 `;
 
