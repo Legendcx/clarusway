8343350b185c583ee33a028559920249644ca95b
burhan yiğiter <burhan.yigiter@gmail.com>
2022-11-23 23:05:19 +0300

    brands

--- Front_End/react/class-notes/rc25_stock-app/stock-app/src/pages/Products.jsx
+++ Front_End/react/class-notes/rc25_stock-app/stock-app/src/pages/Products.jsx
@@ -1,5 +1,57 @@
-const Products = () => {
-  return <div>Products</div>;
+import { useEffect, useState } from "react";
+import useStockCalls from "../hooks/useStockCalls";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import { useSelector } from "react-redux";
+import FirmCard from "../components/FirmCard";
+import FirmModal from "../components/modals/FirmModal";
+import { getSnackbarUnstyledUtilityClass } from "@mui/base";
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const Firms = () => {
+  const { getBrands, getCategories, getProducts } = useStockCalls();
+  const { products } = useSelector((state) => state.stock);
+  const [open, setOpen] = useState(false);
+  const [info, setInfo] = useState({});
+
+  useEffect(() => {
+    getBrands();
+    getCategories();
+    getProducts();
+  }, []);
+
+  return (
+    <Box>
+      <Typography variant="h4" color="error" mb={4}>
+        Products
+      </Typography>
+
+      <Button variant="contained" onClick={() => setOpen(true)}>
+        New Product
+      </Button>
+      {/*
+      <ProductModal open={open} setOpen={setOpen} info={info} setInfo={setInfo} /> */}
+
+      {firms?.length > 0 && (
+        <Grid container justifyContent="center" gap={3}>
+          {firms?.map((firm) => (
+            <Grid item key={firm.id}>
+              <FirmCard firm={firm} setOpen={setOpen} setInfo={setInfo} />
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Box>
+  );
 };
 
-export default Products;
+export default Firms;
