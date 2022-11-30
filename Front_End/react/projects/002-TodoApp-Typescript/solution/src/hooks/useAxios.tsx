2f16d6bbc703aaa08ad4e1b27965091b7a907a1c
burhan yiğiter <burhan.yigiter@gmail.com>
2022-12-01 00:06:59 +0300

    TodoTypescript

---
+++ Front_End/react/projects/002-TodoApp-Typescript/solution/src/hooks/useAxios.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+
+//todo ile ilgili fonksiyonları custom hooksta topladık
+const useAxios = () => {
+    const [todos, setTodos] = useState<TodoType[]>([])//TodoType array olarak kabul ediyor
+    const getTodos = async () => {
+        const { data } = await axios.get<TodoType[]>("https://635a951e6f97ae73a63139c4.mockapi.io/todos")//gelen response a göre belirledik bu ne işe yarıyor gelen datanın fieldlarına erişebiliyoruz
+        // console.log(data)
+        setTodos(data)
+    }
+    const addTodo: AddFn = async (text) => {
+        const newTodo = {
+            task: text,
+            isDone: false
+        }
+        const { data } = await axios.post<TodoType>("https://635a951e6f97ae73a63139c4.mockapi.io/todos", newTodo)
+        if (data) {
+            getTodos()
+        }
+    };
+
+    const toggleTodo: ToggleFn = async (item) => {
+        const updateTodo = {
+            id: item.id,
+            task: item.task,
+            isDone: !item.isDone
+        }
+        const { data } = await axios.put<TodoType>(`https://635a951e6f97ae73a63139c4.mockapi.io/todos/${item.id}`, updateTodo)
+        if (data) {
+            getTodos()
+        }
+    }
+    const deleteTodo: DeleteFn = async (id) => {
+        try {
+            const res = await axios.delete<TodoType>(`https://635a951e6f97ae73a63139c4.mockapi.io/todos/${id}`)
+            console.log(res)
+            getTodos()
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return { todos, getTodos, addTodo, deleteTodo, toggleTodo }
+}
+
+export default useAxios
---
+++ Front_End/react/projects/002-TodoApp-Typescript/solution/src/pages/Home.tsx
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import InputForm from '../components/InputForm'
+import TodoList from '../components/TodoList';
+// import useAxios from '../hooks/useAxios';
+
+const url = "https://635a951e6f97ae73a63139c4.mockapi.io/todos"
+
+
+
+const Home = () => {
+  //custom hooktan da çekip burada çağırıp ve componentlere props olarak geçebiliriz. Bunları ilgili componentlerde de çağırabilirdik. Ama propslar typescriptte nasıl kullanılıyor onu görmek adına bu şekilde kullanabiliriz
+
+  // const { todos, getTodos, addTodo, deleteTodo, toggleTodo } = useAxios()
+
+  const [todos,setTodos] = useState<TodoType[]>([])
+
+  const getTodos = async () => {
+    try {
+      const {data} = await axios.get<TodoType[]>(url)
+      console.log(data)
+      setTodos(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const addTodo:AddFn= async (text) => {
+    const newTodo = {
+      task: text,
+      isDone: false
+    }
+    try {
+      await axios.post(url,newTodo)
+      getTodos()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const toggleTodo:ToggleFn = async (item) => {
+    try {
+      await axios.put(`${url}/${item.id}`,{...item,isDone:!item.isDone})
+      getTodos();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const deleteTodo:DeleteFn = async (id) => {
+    try {
+      await axios.delete(`${url}/${id}`)
+      getTodos();
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(()=>{
+    getTodos()
+  },[])
+  
+
+  return (
+    <div className='main'>
+      <InputForm addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </div>
+  )
+}
+
+export default Home
