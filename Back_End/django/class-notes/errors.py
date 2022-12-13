a5bf5c061100c71a16a8e4c1f0a4412888bb2d78
burhan yiğiter <burhan.yigiter@gmail.com>
2022-12-13 22:48:39 +0300

    error

--- Back_End/django/class-notes/errors.py
+++ Back_End/django/class-notes/errors.py
@@ -88,3 +88,93 @@
 
 #? my_file = open("first_file.txt", encoding="utf-8")
 
+
+
+# sea = open('fishes.txt', 'r')
+# sea.see(5)  # buradan başla demek
+# print(sea.read(21))   # buraya kadar oku demek, hrhangi br şey demeksek oradan okumaya devam ediyor.
+# sea.close()  açtığımız yani okuduğumuz file ı kapatmak zorundayız.
+
+# sea = open('fishes.txt', 'r')
+# print(sea.tell())
+# sea.close()
+
+# rumi = open("rumi.txt")
+# print(rumi.read())
+# rumi.close()
+
+# sea = open("fishes.txt", 'r')
+# print(sea.readline(13))  # tek satır okur 13 sıra ya kadar okur
+# print(sea.readline())
+# print(sea.readline())
+
+# sea.close()
+
+
+# sea = open("fishes.txt", 'r', encoding='UTF-8')
+# print(sea.readlines())   # bütün satırları okur
+
+# sea.close()
+
+# sea = open("fishes.txt", 'r', encoding='UTF-8')
+
+# for line in sea:  #  or for line in sea.readlines() diyebiliriz.
+#   print(line)
+
+# sea.close()
+
+
+
+# with open('fishes.txt', 'r') as sea:
+#   print(sea.read())
+  
+# with ile açtığımız için  kapatmaya gerek yoktur.
+
+
+###!   ==== WRİTE METODU =====
+
+# with open("dummy.txt", 'w') as file:
+#   file.write("This is a first line")
+  
+# with open("dummy.txt") as file:
+#   print(file.read())
+  
+# with open("dummy.txt", 'w') as file:  # tekrar yazarsak öncekini siler yeniden yazar
+#   file.write("This is a first linhdhde")
+
+# fruits = [ "Banana", "apple", "orange", "peach"]
+
+# # with open('fruits.txt', 'w') as file:
+# #   for fruit in fruits:
+# #     file.write(fruit + '\n')
+    
+# with open("fruits.txt", "w") as file:
+#   for fruit in fruits:
+#     file.write(f"{fruit}\n")
+    
+# fruits = [ "Banana\n", "apple\n", "orange\n", "peach\n"]
+
+# with open("fruits.txt", "a") as file:
+ 
+#     file.write("Melon")  #  dosyaya üzerine değil yanına yazdırdık 'a ileappend demektir.
+    
+    
+    
+from datetime import datetime
+
+num1 = 250
+num2 = [1,2,3,4,5,0,20]
+res_list = []
+
+for num in num2:
+  try:
+    res_num = num1/num
+  except Exception as e:  # hatanın tam olarak ne olduğunu bilemezsek bunu yazzdırabilriz
+      with open("error.log", 'a') as file:
+        file.write(f'You have error {e} --->{num} at {datetime.now()}\n')
+  else:
+    res_list.append(res_num)
+  finally:
+    print('This works every time')
+
+print(res_list)
