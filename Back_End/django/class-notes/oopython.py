9f6eb1285b237479e5eb0a0a8f363e121cbb720e
burhan yiğiter <burhan.yigiter@gmail.com>
2022-12-15 23:06:16 +0300

    oop

--- Back_End/django/class-notes/oopython.py
+++ Back_End/django/class-notes/oopython.py
@@ -76,30 +76,114 @@ print_types(test) """
 
 #! SELF keyword and methods
 
-class Person:
-  company = "Jimjim"
+# class Person:
+#   company = "Jimjim"
   
-  def test(self):  # hangi instance üzerinde çalışırsa buradaki self onu temsil ediyor.
-    print("test")
+#   def test(self):  # hangi instance üzerinde çalışırsa buradaki self onu temsil ediyor.
+#     print("test")
     
-  def get_details(self):
-    print(f"{self.name} - {self.age}")
+#   def get_details(self):
+#     print(f"{self.name} - {self.age}")
     
-  def set_details(self, name, age):
-    self.name = name
-    self.age = age
+#   def set_details(self, name, age):
+#     self.name = name
+#     self.age = age
+#   @staticmethod   #  Bu hiçbir instance için değişkeni değiştirmez. cevap sabiittir. Statik metod yapıyor. Statik metodlar parametre almazlar
+#   def salute():
+#     print("Hi there!")
   
-person1 = Person()
-person2 = Person()
-# Person.test(person1) self kelimesi aslında buradan geliyor.
-person1.test()
+# person1 = Person()
+# person2 = Person()
+# # Person.test(person1) self kelimesi aslında buradan geliyor.
+# person1.test()
 
-person1.name = "Burki"
-person1.age = 43
+# person1.name = "Burki"
+# person1.age = 43
 
-person1.get_details()
+# person1.get_details()
 
-# person2.name = "Erkoş"
-# person2.age = 30
-person2.set_details("buralar", 44)
-person2.get_details()
+# # person2.name = "Erkoş"
+# # person2.age = 30
+# person2.set_details("buralar", 44)
+# person2.get_details()
+
+# person1.salute()
+# person2.salute()
+
+#! SPECİALS METODLAR (dunder methods)
+
+# class Person:
+#   company = "Microsoft"
+#   person_count = 0
+  
+  #  init otomatik olarak atanan instanceye giderse hemen çalışır. Default değerde verebiliriz.
+  # def __init__(self, name, age, gender="Male"):
+  #   self.name = name
+  #   self.age = age
+  #   self.gender = gender
+  #   Person.person_count = Person.person_count +1
+    
+  #   def __str__(self):
+  #     return f"{self.name} - {self.age}"
+  
+  # def get_details(self):
+  #    print(f"{self.name} - {self.age}")
+    
+  
+  # def set_details(self, name, age):
+  #   self.name = name
+  #   self.age = age
+    
+# person1 =  Person("Burkiş", 43)
+# person2 =  Person("Burki", 33)
+# person1.get_details()
+# print(Person.person_count)
+
+# person2.get_details()  # dersek çalışma zargüman vermemiz gerekiyor.
+
+
+#! __STR__ METODU 
+#? çıktı olarak string verir özel metodlardır.
+
+# print(person1)
+# print(person2)
+
+#! OOP AŞAMALARI NELERDİR? 
+ #!===  1- Encapsulation  = === 
+ #? kullanıcı tarafından sınıfların, verilerin ve metodların ne kadarının görüntüleneceğini ve ne kadarını değiştirebileceğimiz belirlediğimiz yapıdır.
+
+# class Person:
+#   company = "Microsoft"
+  
+  
+#   def __init__(self, name, age, gender="Male"):
+#     self.name = name
+#     self.age = age
+#     self.gender = gender
+#     self._id = 5000
+#     self.__number = 200
+   
+    
+#     def __str__(self):
+#       return f"{self.name} - {self.age}"
+  
+#   def get_details(self):
+#      print(f"{self.name} - {self.age}")
+     
+# person1 = Person("Burkiş", 50)
+# print(person1._id)  # TEK ALT ÇİZGİ yazarsak ulaşabiliriz. ama sıkıntı olur demektir.
+
+# print(person1.__number)  # İKİ ALT ÇİZGİ İSE ulaşışamaz. hataverir encapsulation yapmayı sağlıyor.
+# print(person1._Person__number)  # BÖYLE yazarsak ulaşabiliriz. veriyi korumak için böyle yazılmıştır.
+
+
+
+#!  ===========   ABSTRACTION   2. ÖZELLİK ===========
+
+# Abstraction, bir nesnenin önemli olan özelliklerini ve davranışlarını ortaya çıkarma işlemidir. Bu sayede nesnenin detayları gizlenir ve sadece temel özellikleri ve davranışları kullanıcılar tarafından görülebilir. Bu sayede nesneler daha basit ve kolay anlaşılır hale gelir. Abstraction, encapsulation ile birlikte kullanılarak kodun daha anlaşılır ve değiştirilebilir hale gelmesine yardımcı olur.
+
+#? Kullanıcıyı gereksiz detaylardan kurtarmal ve ihtiyacı olmayan şeyleri göstermek. Soyutlama yani
+
+liste = [2,3,5,1,4]
+liste.sort(reverse=True)
+print(liste)
