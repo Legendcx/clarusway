7ba8bf532df5bc6b226f5b57d53a376fba106997
burhan yiğiter <burhan.yigiter@gmail.com>
2022-12-29 23:06:57 +0300

    token

--- Back_End/django/class-notes/009_DRF_Auth_Perm/user/serializers.py
+++ Back_End/django/class-notes/009_DRF_Auth_Perm/user/serializers.py
@@ -21,3 +21,21 @@ class REgisteeSerializer(serializers.ModelSerializer):
       'password',
       'password2'
     )
+    
+    
+  def validate(self, data):
+    if data['password'] != data['password2']:
+      raise serializers.ValidationError(
+        {'password': "Password fields didn't match"}
+      )
+      
+    return data
+  
+  def create(self, validated_data):
+    validated_data.pop('password2')
+    password = validated_data.pop('password')
+    user = User.objects.create(**validated_data)
+    user.set_password(password)
+    user.save()
+    return user
+    
--- Back_End/django/class-notes/009_DRF_Auth_Perm/user/views.py
+++ Back_End/django/class-notes/009_DRF_Auth_Perm/user/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
 
+from django.contrib.auth.models import User
+from .serializers import REgisteeSerializer
+from rest_framework.generics import CreateAPIView
+from rest_framework.authtoken.models import Token
+
+
+class RegisterView(CreateAPIView):
+  queryset = User.objects.all()
+  serializer_class = REgisteeSerializer
+  
+  def create(self, request, *args, **kwargs):
+    response = super().create(request, *args, **kwargs)
+    token = Token.objects.create(user_id = response.data['id'])
+    response.data['token'] = token.key
+    # print(response.data)
+    return response
 # Create your views here.
