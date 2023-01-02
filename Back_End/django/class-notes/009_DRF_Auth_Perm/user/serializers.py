4f43f1a7d824908f87fd3f6c0ea0011a7ef2bcf2
burhan yiğiter <burhan.yigiter@gmail.com>
2023-01-02 20:24:36 +0300

    blogapp1

--- Back_End/django/class-notes/009_DRF_Auth_Perm/user/serializers.py
+++ Back_End/django/class-notes/009_DRF_Auth_Perm/user/serializers.py
@@ -4,7 +4,7 @@ from rest_framework.validators import UniqueValidator
 # from django.conf import settings
 
 
-class REgisteeSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
   email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
   password = serializers.CharField(write_only=True)
   password2 = serializers.CharField(write_only= True, required=True)
