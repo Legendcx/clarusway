f112bfc8a2be57e6e36838e0df8efad11b42d2ad
burhan yiğiter <burhan.yigiter@gmail.com>
2023-01-02 22:24:37 +0300

    blog3

--- Back_End/django/class-notes/10-BLOGAPP/blog/views.py
+++ Back_End/django/class-notes/10-BLOGAPP/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Category, Blog
+from .serializer import CategorySerializer, BlogSerializer
+
+
+class CategoryView(ModelViewSet):  # crud
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_fields = ['name']
+    
+    
+class BlogView(ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    filter_fields = ['category']
+    
+    
